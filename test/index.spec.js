0c147fcb31ad49ba44bb2c835944491bbd1ac0df
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-25 12:49:14 -0700

    fix: bumped node version

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,15 @@
 import { expect } from 'chai';
+import { describe } from 'node-tdd';
 import index from '../src/index.js';
 
-describe('Testing Package', () => {
+describe('Testing Package', { useNock: true }, () => {
   it('Testing export', () => {
     expect(typeof index).to.equal('function');
     expect(Object.entries(index)).to.deep.equal([]);
+  });
+
+  it('Testing request', async () => {
+    const r = await index({ url: 'https://www.google.com', method: 'HEAD' });
+    expect(r.status).to.equal(200);
   });
 });
