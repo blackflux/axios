f7630f2d247c4c8eee67bb08232cc5da7eb9ef6a
Lukas Siemon <lukas.siemon@blackflux.com>
2022-07-25 13:15:23 -0700

    fix: errors now redact verbose information

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,10 @@ import { expect } from 'chai';
 import { describe } from 'node-tdd';
 import index from '../src/index.js';
 
-describe('Testing Package', { useNock: true }, () => {
+describe('Testing Package', {
+  useNock: true,
+  nockStripHeaders: true
+}, () => {
   it('Testing export', () => {
     expect(typeof index).to.equal('function');
     expect(Object.entries(index)).to.deep.equal([]);
@@ -11,5 +14,21 @@ describe('Testing Package', { useNock: true }, () => {
   it('Testing request', async () => {
     const r = await index({ url: 'https://www.google.com', method: 'HEAD' });
     expect(r.status).to.equal(200);
+  });
+
+  it('Testing exception format', async ({ capture }) => {
+    const url = 'https://google.com/unknown';
+    const r = await capture(() => index({ url, method: 'HEAD' }));
+    expect(JSON.parse(JSON.stringify(r))).to.deep.equal({
+      message: '',
+      name: 'Error',
+      status: 404,
+      config: {
+        headers: { Accept: 'application/json, text/plain, */*', 'User-Agent': 'axios/0.27.2' },
+        url: 'https://google.com/unknown',
+        method: 'head'
+      },
+      code: 'ERR_BAD_REQUEST'
+    });
   });
 });
